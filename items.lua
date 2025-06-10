fec51f113b10c1b4c90710c2618def7cd05be857
Den <plizzz.healme@gmail.com>
2025-06-10 21:58:29 +0300

    Auto-update: 6/10/2025, 2:58:29 PM

--- items.lua
+++ items.lua
@@ -13,6 +13,7 @@ buylist = {
     {id = 37700, name = "Crystallized Air", price = 35000},
     {id = 41163, name = "Titanium Bar", price = 530000},
     {id = 36910, name = "Titanium Ore", price = 265000},
+    {id = 36913, name = "Saronite Bar", price = 75000},
     {id = 37663, name = "Titansteel Bar", price = 2560000},
     {id = 33470, name = "Frostweave Cloth", price = 8000},
     {id = 41510, name = "Bolt of Frostweave", price = 40000},
