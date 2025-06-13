dacecf27a9b7c4773446c9b980c314a7040f849c
Den <plizzz.healme@gmail.com>
2025-06-13 21:16:31 +0300

    Auto-update: 6/13/2025, 2:16:31 PM

--- items.lua
+++ items.lua
@@ -33,7 +33,7 @@ buylist = {
     {id = 33568, name = "Borean Leather", price = 23000},
     {id = 38425, name = "Heavy Borean Leather", price = 138000},
     {id = 44128, name = "Arctic Fur", price = 1380000},
-    {id = 38557, name = "Icy Dragonscale", price = 72000},
+    {id = 38557, name = "Icy Dragonscale", price = 80000},
     {id = 38558, name = "Nerubian Chitin", price = 20000},
     {id = 36919, name = "Cardinal Ruby", price = 1500000},
     {id = 36934, name = "Eye of Zul", price = 600000},
