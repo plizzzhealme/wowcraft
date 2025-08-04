977efdd52d1f40642efe92d95d19c7ec85e1ada6
Den <plizzz.healme@gmail.com>
2025-08-04 09:11:17 +0300

    Update core.lua

--- core.lua
+++ core.lua
@@ -50,24 +50,6 @@ SlashCmdList["FUCK"] = function(msg)
         end
     end
 end
-function GetColoredMoney(copper)
-    local gold = math.floor(copper / 10000)
-    local silver = math.floor((copper % 10000) / 100)
-    local copper = copper % 100
-    
-    -- Classic WoW color codes
-    local goldColor = "|cFFFFD700"   -- Gold (yellow)
-    local silverColor = "|cFFC0C0C0" -- Silver (light gray)
-    local copperColor = "|cFFB87333" -- Copper (orange-brown)
-    
-    return string.format(
-        "%s%02d%s%02d%s%02d|r",
-        goldColor, gold,
-        silverColor, silver,
-        copperColor, copper
-    )
-end
-
 
 SLASH_BUY1 = "/buy"
 SlashCmdList["BUY"] = function() 
