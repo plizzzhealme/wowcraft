b36eb086afbc45da0ec1e37673c0a81a2591e08f
Den <plizzz.healme@gmail.com>
2025-08-05 16:08:20 +0300

    Update util.lua

--- util.lua
+++ util.lua
@@ -1,21 +1,3 @@
-function GetColoredMoney(copper)
-    local gold = math.floor(copper / 10000)
-    local silver = math.floor((copper % 10000) / 100)
-    local copper = copper % 100
-    
-    -- Classic WoW color codes
-    local goldColor = "|cFFFFD700"   -- Gold (yellow)
-    local silverColor = "|cFFC0C0C0" -- Silver (light gray)
-    local copperColor = "|cFFB87333" -- Copper (orange-brown)
-    
-    return string.format(
-        "%s%02d%s%02d%s%02d|r",
-        goldColor, gold,
-        silverColor, silver,
-        copperColor, copper
-    )
-end
-
 function GetMoneyString(money)
 	local gold = floor(money / 10000)
 	local silver = floor((money - gold * 10000) / 100)
@@ -28,4 +10,3 @@ function GetMoneyString(money)
 		return format(COPPER_AMOUNT_TEXTURE, copper, 0, 0)
 	end
 end
-
