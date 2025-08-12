cbc16d316995e3fb38e3e26e4c248d7df1ee5fb0
Den <plizzz.healme@gmail.com>
2025-08-12 13:03:22 +0300

    Update auction.lua

--- auction.lua
+++ auction.lua
@@ -12,6 +12,12 @@ function BuyBid(msg)
         local itemLink = GetAuctionItemLink("list", i)
         local itemId = itemLink and itemLink:match("item:(%d+):") or nil
         
+        local testBid = getBidAmount(i, overbidProtection)
+        
+        if testBid then
+            print(GetMoneyString(testBid))
+        end
+        
         if itemId then
             itemId = tonumber(itemId)
         end
@@ -22,8 +28,8 @@ function BuyBid(msg)
             local nextBid = math.max(minBid, bidAmount) + minIncrement
             local bidCost = nextBid / count
             local maxPrice = cost * count
-            local smartBid = maxPrice / overbidProtection
-            local minPrice = smartBid
+            local safeBid = maxPrice / overbidProtection
+            local minPrice = safeBid
             
             if buyoutPrice > 0 then
                 minPrice = math.min(minPrice, buyoutPrice / BID_FACTOR)
@@ -41,4 +47,38 @@ function BuyBid(msg)
             end
         end
     end
+end
+
+local function getBidAmount(i, overbidProtection)
+    local _, _, count, _, _, _, minBid, minIncrement, buyoutPrice, bidAmount, highestBidder, _, _ = GetAuctionItemInfo("list", i)
+    local itemLink = GetAuctionItemLink("list", i)
+    local itemId = itemLink and itemLink:match("item:(%d+):") or nil
+    local itemCost = GetCost(itemId)
+    local nextBid = math.max(minBid, bidAmount) + minIncrement
+    local bidCost = nextBid / count
+    
+    --stop if over buylist price
+    if bidCost > itemCost then
+        return
+    end
+    
+    local buyoutCost = buyoutPrice / count
+    
+    --instant buyout if possible
+    if buyoutCost <= itemCost then
+        return buyoutPrice
+    end
+    
+    --skip auctions we have bids on
+    if highestBidder then
+        return
+    end
+    
+    local safeBid = math.max(nextBid, count * itemCost / overbidProtection)
+    
+    if not buyoutPrice then
+        return safeBid
+    end
+    
+    return math.min(safeBid, buyoutPrice / BID_FACTOR)
 end
