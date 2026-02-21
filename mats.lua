9a2e3e0f2e55a8c3683a19ca05a92c0e2449b565
Den <plizzz.healme@gmail.com>
2026-02-21 17:02:33 +0300

    Auto-update mats.lua: 2/21/2026, 9:02:33 AM

--- mats.lua
+++ mats.lua
@@ -67,6 +67,6 @@ MAT = {
     [38426] = 24000,  --Eternium Thread
     [47556] = 1200000,  --Crusader Orb
     [43102] = 400000,  --Frozen Orb
-    [45087] = 2750000,  --Runed Orb
+    [45087] = 1600000,  --Runed Orb
     [49908] = 3750000,  --Primordial Saronite
 }
